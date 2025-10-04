49e0ba383be8ff6e8f27cfdd076ee0d921771f43
midestic <badmususman50@gmail.com>
2025-10-04 09:53:44 +0100

    fix

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="text-center  w-full h-[100vh] flex justify-center items-center">
+      <Text className="text-red-500">Welcome</Text>
+    </View>
+  );
+}
