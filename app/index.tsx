6f0fb2f26e3043a374711c93756279c26380330e
midestic <badmususman50@gmail.com>
2025-10-04 10:03:37 +0100

    native wind setup

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="text-center  w-full h-[100vh] flex justify-center items-center">
-      <Text className="text-red-500">Welcome</Text>
+      <Text className="text-[40px] text-accent">Welcome</Text>
     </View>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,23 @@ module.exports = {
   ],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#030014",
+        secondary: "#151312",
+        light: {
+          100: "#D6C6FF",
+          200: "#A885db",
+          300: "#9ca4ab",
+        },
+
+        dark: {
+          100: "#221f3d",
+          200: "#0f0d23",
+        },
+        accent: "#ab8bff",
+      },
+    },
   },
   plugins: [],
 };
