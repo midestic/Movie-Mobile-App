8707e594bd4edaa690d1eee9d63c05d4b9bbf392
midestic <badmususman50@gmail.com>
2025-10-04 12:50:35 +0100

    routing

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,12 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View className="text-center  w-full h-[100vh] flex justify-center items-center">
       <Text className="text-[40px] text-accent">Welcome</Text>
+
+      <Link href={"/onboarding"}>onboarding</Link>
     </View>
   );
 }
