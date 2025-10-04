df4860663c305d7d55a98f3863d144bb3283c3dd
midestic <badmususman50@gmail.com>
2025-10-04 14:41:51 +0100

    tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Text, View } from "react-native";
 
-const onboarding = () => {
+const _layout = () => {
   return (
     <View>
-      <Text>onboarding</Text>
+      <Text>_layout</Text>
     </View>
   );
 };
 
-export default onboarding;
+export default _layout;
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,9 @@
-import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View className="text-center  w-full h-[100vh] flex justify-center items-center">
       <Text className="text-[40px] text-accent">Welcome</Text>
-
-      <Link href={"/onboarding"}>onboarding</Link>
     </View>
   );
 }
